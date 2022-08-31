6a0256270af498c2535733e9ff58c4f48865cd2c
kim7881 <rlawls7881@gmail.com>
2022-08-31 17:50:29 +0900

    hahah

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:actual/layout/default_layout.dart';
 import 'package:actual/screen/auto_dispose_modifier_screen.dart';
 import 'package:actual/screen/family_modifier_screen.dart';
 import 'package:actual/screen/future_provider_screen.dart';
+import 'package:actual/screen/listen_provider_screen.dart';
 import 'package:actual/screen/state_notifier_provider_screen.dart';
 import 'package:actual/screen/state_provider_screen.dart';
 import 'package:actual/screen/stream_provider_screen.dart';
@@ -75,6 +76,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => ListenProviderScreen(),
+                ),
+              );
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),
